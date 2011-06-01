cf8230695cd906f437eb8d3a24eb4d9bfc7b86a1
Benjamin Teuber <bsteuber@gmail.com>
2011-06-01 13:11:27 +0200

    mapp and mapc

--- src/libs/fn.clj
+++ src/libs/fn.clj
@@ -3,3 +3,18 @@
 (defn funcall [f arg]
   (f arg))
 
+;;; from David Cabana's blog
+;;; http://erl.nfshost.com/2011/05/22/map-mapp-and-mapc/
+
+(defn mapp
+  ([f] (partial map f))
+  ([f x & args]
+     (apply map (partial f x)
+            args )))
+
+(defn mapc [& args]
+  (let [[fns xs] (partition-by fn? args)
+        g (apply comp fns)]
+    (if (empty? xs)
+      (partial map g)
+      (apply map g xs ))))

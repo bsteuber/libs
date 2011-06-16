b087fa2dcb1e91b58575e1b5009b7bc3ed637981
Benjamin Teuber <bsteuber@gmail.com>
2011-06-16 20:29:01 +0200

    future* macro

--- src/libs/debug.clj
+++ src/libs/debug.clj
@@ -1,5 +1,6 @@
 (ns libs.debug
-  (:use (libs log))
+  (:use (clojure stacktrace)
+        (libs log))
   (:require [clojure.string :as str]))
 
 (defmacro dbg [x]
@@ -10,3 +11,13 @@
 (defn fail [& args]
   (apply error args)
   (throw (RuntimeException. (str/join " " args))))
+
+;; Overwrite this using a hook
+(defn handle-error [e]
+  (let [trace ((with-out-str (print-cause-trace e)))]
+    (fail trace)))
+
+(defmacro future* [& body]
+  `(future (try ~@body
+                (catch Throwable e#
+                  (handle-error e#)))))

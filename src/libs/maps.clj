12ffacf8ea93bb8e9197a56b965a4be1adaea82b
Benjamin Teuber <bsteuber@gmail.com>
2011-07-08 18:54:56 +0200

    map patch/diff

--- src/libs/maps.clj
+++ src/libs/maps.clj
@@ -4,3 +4,26 @@
   (into {} (map (fn [[x y]]
                   [y x])
                 m)))
+
+(defn patch-map [m1 m2]
+  (merge-with #(if (and (map? %1)
+                        (map? %2))
+                 (patch-map %1 %2)
+                 %2)
+              m1
+              m2))
+
+(defn diff-map [m1 m2]
+  (reduce (fn [m [key val]]
+            (let [orig (m key)]
+              (cond (= orig val)
+                    (dissoc m key)
+                    ;;
+                    (and (map? orig)
+                         (map? val))
+                    (assoc m key (diff-map orig val))
+                    ;;
+                    :else
+                    m)))
+          m1
+          m2))
--- test/test/libs/maps.clj
+++ test/test/libs/maps.clj
@@ -5,3 +5,16 @@
 (fact (invert-map {}) => {}
       (invert-map {:a 2}) => {2 :a}
       (invert-map {:a 3, :b 4}) => {3 :a, 4 :b})
+
+(fact (patch-map {} {}) => {}
+      (patch-map {:a 1} {}) => {:a 1}
+      (patch-map {} {:a 1}) => {:a 1}
+      (patch-map {:b 2} {:a 1}) => {:a 1 :b 2}
+      (patch-map {:a {:b 1 :c 1}} {:a {:c 2}}) => {:a {:b 1 :c 2}})
+
+(fact (diff-map {} {}) => {}
+      (diff-map {} {:a 1}) => {}
+      (diff-map {:a 1} {}) => {:a 1}
+      (diff-map {:a 1 :b 2} {:a 1}) => {:b 2}
+      (diff-map {:a {:b 1 :c 2}} {:a {:b 1}}) => {:a {:c 2}}
+      (diff-map {:a {:b 1 :c 2}} {:a {:b 1 :c 2}}) => {})
